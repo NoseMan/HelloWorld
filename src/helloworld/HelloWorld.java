686852f81d00e8453df5a6d6a3de016fff54c828
g13942sy <g13942sy@s30449.ACTSUDA>
2016-04-08 11:19:44 +0900

    HelloWorldって出力します

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author g13942sy
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //ハローワールド出力
+        System.out.println("Hello,world");
+    }
+    
+}
